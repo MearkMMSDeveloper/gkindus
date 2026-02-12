ee6ed3f392538e94745729efc78225a53d7e639a
MearkMMSDeveloper <meark.marketer@gmail.com>
2026-02-12 16:04:42 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/gk-indus/",
   server: {
     host: "::",
     port: 8080,
