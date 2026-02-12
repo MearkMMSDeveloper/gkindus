f00539b35122a4e80d796285fa4b184952592661
MearkMMSDeveloper <meark.marketer@gmail.com>
2026-02-12 16:19:18 +0530

    Set BrowserRouter basename based on environment

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.PROD ? "/gkindus" : "/"}>
         <Layout>
           <Routes>
             <Route path="/" element={<Index />} />
