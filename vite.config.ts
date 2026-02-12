b9b49114c52f9f8462f88838498a3875ebe12dde
MearkMMSDeveloper <meark.marketer@gmail.com>
2026-02-12 15:33:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/gk-indus/",
   server: {
     host: "::",
     port: 8080,
