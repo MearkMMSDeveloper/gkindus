f75dacab71b49d2c4e393adbced1d2ec8627333c
MearkMMSDeveloper <meark.marketer@gmail.com>
2026-02-12 16:06:13 +0530

    Change base path from '/gk-indus/' to '/gkindus/'

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/gk-indus/",
+  base: "/gkindus/",
   server: {
     host: "::",
     port: 8080,
