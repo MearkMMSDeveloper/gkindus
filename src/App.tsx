1c2bfc7dd00fb945ea6065cfa33321155a70c2ef
MearkMMSDeveloper <meark.marketer@gmail.com>
2026-02-12 15:34:16 +0530

    Set basename for BrowserRouter to '/gk-indus'

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/gk-indus">
         <Layout>
           <Routes>
             <Route path="/" element={<Index />} />
